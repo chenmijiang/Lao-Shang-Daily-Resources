cd2674fa0ed55b2149f177b5e4dbcf84382a8b19
jack <jack.chenyuana@qq.com>
2022-12-29 15:35:41 +0800

    添加简要注释

--- src/utils/md.js
+++ src/utils/md.js
@@ -7,14 +7,15 @@ module.exports.writeMD = function (docsMap, mdPath, count = 15) {
   // 逆序排序
   let keys = Object.keys(docsMap).sort().reverse();
   for (let i = 0; i < keys.length; i++) {
-    // 超过 15 天的数据进行折叠处理
+    // 超过一定天数的数据进行折叠处理
     if (i < count) {
       content.push(`## ${keys[i]}`);
     } else {
       content.push(`<details><summary>${keys[i]}</summary>`);
     }
+
     for (const value of docsMap[keys[i]]) {
-      content.push(`- [${value}](./${keys[i]}/${value.replaceAll(' ', '%20')})`);
+      content.push(`- [${value}](./${keys[i].slice(0, 4)}/${keys[i]}/${value.replaceAll(' ', '%20')})`);
     }
     i >= count ? content.push('</details>') : '';
   }
--- src/utils/time.js
+++ src/utils/time.js
@@ -1,12 +1,10 @@
 const moment = require('moment')  //引入moment
 
-// 获取时间格式 xxxx-xx-xx （默认）
-function getCurrentTime(formats = 'YYYY-MM-DD') {
+// 获取时间，格式 xxxx-xx-xx （默认）
+module.exports.getCurrentTime = function (formats = 'YYYY-MM-DD') {
   moment.locale('zh-cn');
 
   let time = moment().format(formats);
 
   return time || 'others';
 }
-
-module.exports.getCurrentTime = getCurrentTime;

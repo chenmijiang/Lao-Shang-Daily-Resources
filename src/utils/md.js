f6c074e97791e552de3b96de1c95511af49f051b
jack <jack.chenyuana@qq.com>
2022-12-29 10:30:58 +0800

    添加折叠策略

--- src/utils/md.js
+++ src/utils/md.js
@@ -1,17 +1,22 @@
 const fs = require('fs');
-const path = require('path');
 
 // 内容写入到 md 文件中
-module.exports.writeMD = function (docsMap, mdPath) {
+module.exports.writeMD = function (docsMap, mdPath, count = 15) {
   // 内容
   let content = ['# 老尚每日资料分享'];
   // 逆序排序
   let keys = Object.keys(docsMap).sort().reverse();
-  for (const key of keys) {
-    content.push(`## ${key}`);
-    for (const value of docsMap[key]) {
-      content.push(`- [${value}](./${key}/${value.replaceAll(' ','%20')})`);
+  for (let i = 0; i < keys.length; i++) {
+    // 超过 15 天的数据进行折叠处理
+    if (i < count) {
+      content.push(`## ${keys[i]}`);
+    } else {
+      content.push(`<details><summary>${keys[i]}</summary>`);
     }
+    for (const value of docsMap[keys[i]]) {
+      content.push(`- [${value}](./${keys[i]}/${value.replaceAll(' ', '%20')})`);
+    }
+    i >= count ? content.push('</details>') : '';
   }
 
   content = content.join('\n\n');
